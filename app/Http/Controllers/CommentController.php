202c769a6e78fded6c61f5e77b255d71f698adca
Maciek <maciejszalek84@gmail.com>
2020-05-19 19:55:59 +0200

    controllers finished, auth ui created

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Comments;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $input['form_user'] = $request->user()->id;
+        $input['on_post'] = $request->input('on_post');
+        $input['body'] = $request->input('body');
+        $slug = $request->input('slug');
+        Comments::create($input);
+        return redirect($slug)->with('message', 'Comment published');
+    }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -47,4 +47,76 @@ class PostController extends Controller
         $post->save();
         return redirect('edit/'.$post->slug)->withMessage($message);
     }
+
+    public function show($slug)
+    {
+        $post = Posts::where('slug', $slug)->first();
+        if(!$post)
+        {
+            return redirect('/')->withErrors('Requested page not found');
+        }
+        $comments = $post->comments;
+        return view('posts.show')->withPost($post)->withComments($comments);
+    }
+
+    public function edit(Request $request, $slug)
+    {
+        $post = Posts::where('slug', $slug)->first();
+        if($post && ($request->user()->id == $post->author_id || $request->user()->is_admin()))
+        {
+            return view('posts.edit')->with('post', $post);
+        }
+        return redirect('/')->withErrors('You have not sufficient permission');
+    }
+
+    public function update(Request $request)
+    {
+        $post_id = Posts::find('post_id');
+        $post = Posts::find($post_id);
+        if($post && ($post->author_id == $request->user()->id || $request->user()->is_admin()))
+        {
+            $title = $request->input('title');
+            $slug = Str::slug($title);
+            $duplicate = Posts::where('slug', $slug)->first();
+            if($duplicate)
+            {
+                if($duplicate->id != $post_id)
+                {
+                    return redirect('edit/'.$post->slug)->withErrors('Title already exists.')->withInput();
+                } else {
+                    $post->slug = $slug;
+                }
+            }
+            $post->title = $title;
+            $post->body = $request->input('body');
+
+            if($request->has('save'))
+            {
+                $post->active = 0;
+                $message = 'Post saved successfully';
+                $landing = $post->slug;
+            } else {
+                $post->active = 1;
+                $message = 'Post update successfully';
+                $landing = $post->slug;
+            }
+            $post->save();
+            return redirect($landing)->withMessage($message);
+        } else {
+            return redirect('/')->withErrors('You have not sufficient permission');
+        }
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $post = Posts::find($id);
+        if($post && ($post->author_id == $request->user()->id || $request->user()->is_admin()))
+        {
+            $post->delete();
+            $data['message'] = 'Post deleted successfully';
+        } else {
+            $data['errors'] = 'Invalid Operation. You have not sufficient permission';
+        }
+        return redirect('/')->with($data);
+    }
 }
